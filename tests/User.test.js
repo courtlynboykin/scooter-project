d4604b77725c07ccc0a0aa643a79d1faa96c742a
“courtlynboykin” <courtlynb@google.com>
2023-09-28 18:19:37 -0400

    initial commit

--- tests/User.test.js
+++ tests/User.test.js
@@ -9,11 +9,28 @@ describe("User property tests", () => {
     expect(typeof user.username).toBe("string");
   })
   // test password
-
+  it("tests password", () => {
+    expect(user.password).toBe("test123")
+  })
   // test age
+  it("returns their age" , () => {
+    expect(user.age).toBe(21)
+  })
 })
 
 
 // test login
+  describe("loggedIn functions", () => {
+    it("is user logged in", () => {
+      user.login("test123");
+      expect(user.loggedIn).toBe(true)
+    })
+  })
 
 // test logout
+describe("logs user out", () => {
+  it("is user logged out", () => {
+    user.logout();
+  expect(user.loggedIn).toBe(false);
+  })
+})
